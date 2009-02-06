090748b36fc7af19264ae48a69a60a3f1d10205c
raytiley <raytiley@gmail.com>
2009-02-06 09:07:02 -0700

    Added some comments

--- devicestatus.php
+++ devicestatus.php
@@ -20,16 +20,42 @@ $twitter_psw		='password';  //twitter password
 require_once('nusoap.php');  //nusoap.php is required to make soap calls to web service
 require('twitterAPI.php');  //twitterAPI.php sends messages to Twitter if you want to do that.
 
-function stringFromInteger($str) {    $len = strlen($str);       for ($i = 0, $int = ''; $i < $len; $i++) {        if (is_numeric($str[$i])) {            $int .= $str[$i];        } elseif(!is_numeric($str[$i]) && $int) {            $i = $len;        }           }    return (int) $int;}
+function stringFromInteger($str) {
+
+    $len = strlen($str);   
+
+    for ($i = 0, $int = ''; $i < $len; $i++) {
+
+        if (is_numeric($str[$i])) {
+
+            $int .= $str[$i];
+
+        } elseif(!is_numeric($str[$i]) && $int) {
+
+            $i = $len;
+
+        }       
+
+    }
+
+    return (int) $int;
+
+}
 
 
 $cablecast_ip = "192.168.0.100";  //Change to your Cablecast Servers IP Address or DNS name
 
 $device_list ="184,170,171,185";  //In Cablecast Device List Mouse over and get number from status bar.
 
-$client = new nusoap_client("http://$cablecast_ip/CablecastWS/CablecastWS.asmx?WSDL", 'wsdl');  // Creates New SOAP client using WSDL file$Channel_4 = '';
-$requestAddress = "http://$cablecast_ip/Cablecast/Autopilot.ForceEvent.Data.aspx?InitialRequest=false&GetAllDeviceStatus=$device_list&LocationID=22";$xml_str = file_get_contents($requestAddress,0);   
-$p = xml_parser_create();xml_parse_into_struct($p, $xml_str, $vals, $index);xml_parser_free($p);
+$client = new nusoap_client("http://$cablecast_ip/CablecastWS/CablecastWS.asmx?WSDL", 'wsdl');  // Creates New SOAP client using WSDL file
+$Channel_4 = '';
+$requestAddress = "http://$cablecast_ip/Cablecast/Autopilot.ForceEvent.Data.aspx?InitialRequest=false&GetAllDeviceStatus=$device_list&LocationID=22";
+$xml_str = file_get_contents($requestAddress,0);
+   
+//This bit parses the XML from Autopilot.ForceEvent.Data.aspx
+$p = xml_parser_create();
+xml_parse_into_struct($p, $xml_str, $vals, $index);
+xml_parser_free($p);
 
 //Enter all the Server checks here
 //You need a block of server code for each device.  I'll eventually make this into an automated loop.
@@ -115,20 +141,59 @@ $requestAddress = "http://$cablecast_ip/Cablecast/Autopilot.ForceEvent.Data.aspx
 
 //End Server 4 code
 
-//Do a bunch of silly time calculations$offset = 0;  //set this if time of your server is different from Cablecast machine.$day = 60*60*24;  //Calculate # of seconds in a day$currentDay = date("Y-m-d")."T00:00:00";$currentDayTime =  date("Y-m-d")."T".date("H:i:s");$convertedDayTime = strtotime($currentDayTime);$searchTimestr = $convertedDayTime-$day+($offset * 60 * 60);$searchTime = date("Y-m-d", $searchTimestr)."T".date("H:i:s", $searchTimestr);
+
+
+//Do a bunch of silly time calculations
+$offset = 0;  //set this if time of your server is different from Cablecast machine.
+$day = 60*60*24;  //Calculate # of seconds in a day
+$currentDay = date("Y-m-d")."T00:00:00";
+$currentDayTime =  date("Y-m-d")."T".date("H:i:s");
+$convertedDayTime = strtotime($currentDayTime);
+$searchTimestr = $convertedDayTime-$day+($offset * 60 * 60);
+$searchTime = date("Y-m-d", $searchTimestr)."T".date("H:i:s", $searchTimestr);
+
+
 //Enter code for Channel Checking here
-//Begin Code for First Channel$gotShow = '';	//  Get Todays Schedule	$result = $client->call('GetScheduleInformation', array(	'ChannelID'        => 1, // Set This to the Channel ID	'FromDate'         => $currentDay,	'ToDate'           => $currentDay,	'restrictToShowID' => 0), '', '', false, true);
 
-	$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
+//Begin Code for First Channel
+$gotShow = '';
 
-	if($resultNumber > 1)		{
-		$count = 0;		while ($count <= ($resultNumber - 1))			{
-			$beginingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']);			$endingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['EndTime']);
+	//  Get Todays Schedule
+	$result = $client->call('GetScheduleInformation', array(
+	'ChannelID'        => 1, // Set This to the Channel ID
+	'FromDate'         => $currentDay,
+	'ToDate'           => $currentDay,
+	'restrictToShowID' => 0), '', '', false, true);
+
+
+
+
+	$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
+
+
+	if($resultNumber > 1)
+		{
+		$count = 0;
+		while ($count <= ($resultNumber - 1))
+			{
+
+			$beginingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']);
+			$endingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['EndTime']);
 			
-			//Check To see if a show is scheduled to play right now			if(($beginingTime <= ($convertedDayTime + ($offset * 60 * 60))) && ($endingTime > ($convertedDayTime + ($offset * 60 * 60))))				{
+			//Check To see if a show is scheduled to play right now
+			if(($beginingTime <= ($convertedDayTime + ($offset * 60 * 60))) && ($endingTime > ($convertedDayTime + ($offset * 60 * 60))))
+				{
 				$testNumber = $count;  //sets testNumber to show Id of currently scheduled show
-				$gotShow = TRUE;  //Sets a switch to force compairson to video server playback				}			$count++;			}
-		if ($gotShow != NULL)  // If a show is currently scheduled, check to see if a device is playing it.			{			if(array_search($result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID'], $server_playing) === FALSE)  //If show is not in list do some stuff
+				$gotShow = TRUE;  //Sets a switch to force compairson to video server playback
+				}
+
+			$count++;
+			}
+
+		if ($gotShow != NULL)  // If a show is currently scheduled, check to see if a device is playing it.
+
+			{
+			if(array_search($result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID'], $server_playing) === FALSE)  //If show is not in list do some stuff
 				{ 
 				echo "Send an alert to twitter"; 
 				$twitter_message = "Show ID ".$result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID']. " is not playing correctly on Channel 4.\nVar Dump of Result\n".print_r($result, true)."\n\nVar Dump of Server_playing\n\n".print_r($server_playing, true)."\n\nEnd Report";
@@ -140,24 +205,51 @@ $requestAddress = "http://$cablecast_ip/Cablecast/Autopilot.ForceEvent.Data.aspx
 				echo "The show is playing correctly on channel 4"; 
 				}
 
-			}		}
+			}
+		}
 //End Code for First Channel
 
 
 
-//Begin Code for Second Channel$gotShow = '';	//  Get Todays Schedule	$result = $client->call('GetScheduleInformation', array(	'ChannelID'        => 2, // Set This to the Channel ID	'FromDate'         => $currentDay,	'ToDate'           => $currentDay,	'restrictToShowID' => 0), '', '', false, true);
+//Begin Code for Second Channel
+$gotShow = '';
 
-	$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
+	//  Get Todays Schedule
+	$result = $client->call('GetScheduleInformation', array(
+	'ChannelID'        => 2, // Set This to the Channel ID
+	'FromDate'         => $currentDay,
+	'ToDate'           => $currentDay,
+	'restrictToShowID' => 0), '', '', false, true);
 
-	if($resultNumber > 1)		{
-		$count = 0;		while ($count <= ($resultNumber - 1))			{
-			$beginingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']);			$endingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['EndTime']);
+
+
+
+	$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
+
+
+	if($resultNumber > 1)
+		{
+		$count = 0;
+		while ($count <= ($resultNumber - 1))
+			{
+			$beginingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']);
+			$endingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['EndTime']);
 		
-			//Check To see if a show is scheduled to play right now			if(($beginingTime <= ($convertedDayTime + ($offset * 60 * 60))) && ($endingTime > ($convertedDayTime + ($offset * 60 * 60))))				{
+			//Check To see if a show is scheduled to play right now
+			if(($beginingTime <= ($convertedDayTime + ($offset * 60 * 60))) && ($endingTime > ($convertedDayTime + ($offset * 60 * 60))))
+				{
 				$testNumber = $count;  //sets testNumber to show Id of currently scheduled show
-				$gotShow = TRUE;  //Sets a switch to force compairson to video server playback				}			$count++;			}
-		if ($gotShow != NULL)  // If a show is currently scheduled, check to see if a device is playing it.			{
-			if(array_search($result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID'], $server_playing) === FALSE)  //If show is not in list do some stuff
+				$gotShow = TRUE;  //Sets a switch to force compairson to video server playback
+				}
+
+			$count++;
+			}
+
+		if ($gotShow != NULL)  // If a show is currently scheduled, check to see if a device is playing it.
+
+			{
+
+			if(array_search($result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID'], $server_playing) === FALSE)  //If show is not in list do some stuff
 				{ 
 				echo "Send an alert to twitter";
 				$twitter_message = "Show ID ".$result['GetScheduleInformationResult']['ScheduleInfo'][$testNumber]['ShowID']. " is not playing correctly on Channel 2.\nVar Dump of Result\n".print_r($result, true)."\n\nVar Dump of Server_playing\n\n".print_r($server_playing, true)."\n\nEnd Report";
@@ -169,7 +261,8 @@ $requestAddress = "http://$cablecast_ip/Cablecast/Autopilot.ForceEvent.Data.aspx
 				echo "<br>The show is playing correctly on channel 2"; 
 				}
 
-			}		}
+			}
+		}
 //End Code for Second Channel
 
 echo "</body></html>";
