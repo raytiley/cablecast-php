ba15528f95ab660753e8eb903a0a99b0afd149b7
Ray Tiley <raytiley@gmail.com>
2012-12-22 14:30:45 -0600

    Cleaned up VODdisplay.php
    Removed nusoap dependency and cleaned up script a bit

--- VODdisplay.php
+++ VODdisplay.php
@@ -1,52 +1,47 @@
 <?php
-/*  This script prints an html table of all shows that are availalbe for VOD on the server
+//Configure
+$server = "pittsfieldtv.dyndns.org";
+date_default_timezone_set('America/New_York');
+// End Configure
 
-The shows must have valid event dates that are in the past
-
-*/
-
-require_once('nusoap.php');  //SOAP function library
-
-$client = new nusoap_client("http://YOURSERVER/CablecastWS/CablecastWS.asmx?WSDL", 'wsdl');  // Creates New SOAP client using WSDL file
+$client = new SoapClient("http://" . $server . "/CablecastWS/CablecastWS.asmx?WSDL");  // Creates New SOAP client using WSDL file
  
 $searchDate = date("Y-m-d")."T12:00:00";
 
 // Search for all shows that have an event date less than now that are available for VOD
-$result = $client->call('AdvancedShowSearch', array(
+$result = $client->AdvancedShowSearch(array(
     'ChannelID'        => 1,
     'searchString'         => '',
-    'eventDate'           => $searchDate,
+    'eventDate'           => date("Y-m-d") . "T00:00:00",
     'dateComparator'      => '<',
     'restrictToCategoryID'  => 0,  
     'restrictToProducerID'   => 0, 
     'restrictToProjectID'    =>  0,
     'displayStreamingShowsOnly'   =>  1,
-    'searchOtherSites'     =>   0,), '', '', false, true);
+    'searchOtherSites'     =>   0,));
 
-
-$resultNumber = count($result['AdvancedShowSearchResult']['SiteSearchResult']['Shows']['ShowInfo']);  // of shows for loop	
-$count = 0; // Set count varaible
-
-
-if($resultNumber == '0')
-{
-//There is probably something wrong if this shows up.
-echo "There are now Shows currently available for on demand viewing.";
-}
-// Prints out a table with time and show title with link to show detial page
-if($resultNumber >= '1')
-{	echo "<table border ='0' width='100%'><tr>\n
-			<th>Program Title</th><th>Link</th></tr>\n";
-	while ($count <= ($resultNumber -1))
-{
-
-echo "<tr><td>".$result['AdvancedShowSearchResult']['SiteSearchResult']['Shows']['ShowInfo'][$count]['Title']."</td><td><a href='".$result['AdvancedShowSearchResult']['SiteSearchResult']['Shows']['ShowInfo'][$count]['StreamingFileURL']."'>Watch Now</a></td></tr>\n";
-
-$count++;
-} 
-echo "</table>";
-
-
+if(!isset($result->AdvancedShowSearchResult->SiteSearchResult->Shows->ShowInfo)) {
+    $vods = array();
+} else {
+    $vods = is_array($result->AdvancedShowSearchResult->SiteSearchResult->Shows->ShowInfo) ?
+        $result->AdvancedShowSearchResult->SiteSearchResult->Shows->ShowInfo :
+        array($result->AdvancedShowSearchResult->SiteSearchResult->Shows->ShowInfo);
 }
 
-?>
+if(count($vods) == '0') {
+    //There is probably something wrong if this shows up.
+    echo "There are now Shows currently available for on demand viewing.";
+} else {
+    // Prints out a table with time and show title with link to show detial page
+    echo "<table>\n";
+    echo "<th>Program Title</th><th>Link</th></tr>\n";
+    
+    foreach($vods as $vod) {
+        echo "<tr>\n";
+        echo "<td>" . $vod->Title . "</td>\n";
+        echo "<td><a href='" . $vod->StreamingFileURL . "'>Watch Now</a></td>";
+        echo "</tr>\n";
+    }
+    
+    echo "</table>";
+}
