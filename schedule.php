70739cc892b7f8b0f1f2ed744d3a4e5ab07a243b
Ray Tiley <raytiley@gmail.com>
2012-12-22 10:22:23 -0600

    Cleaned up schedule.php
    Removed nusoap dependency in favor of built in SOAP
    
    Also made the script a little bit cleaner

--- schedule.php
+++ schedule.php
@@ -6,80 +6,54 @@ $displayDays = 7;  //Number of Days to Display
 date_default_timezone_set('America/New_York');
 //End Configure
  
-require_once('nusoap.php');
-$client = new nusoap_client($server."CablecastWS/CablecastWS.asmx?WSDL", 'wsdl'); // Creates New SOAP client using WSDL file
+
+$client = new SoapClient($server."CablecastWS/CablecastWS.asmx?WSDL"); // Creates New SOAP client using WSDL file
  
-if (isset($_GET['d']) && isset($_GET['m']) && (isset($_GET['y']))) 
-{
-	$dateString = $_GET['y']."-".$_GET['m']."-".$_GET['d']."T00:00:00";
+// Get year month and day from individual query string parameters if exist.
+// There must of been some reason I did it this way. Probably some crappy calendar
+// I was using at the time. Feel free to change to a better calendar.
+if (isset($_GET['d']) && isset($_GET['m']) && (isset($_GET['y']))) {
+	$dateString = $_GET['y'] . "-" . $_GET['m'] . "-" . $_GET['d'];
+} else {
+	//default to today's date
+	$dateString = date("Y-m-d");
 }
+//Calculate the end date
+$endDate = date("Y-m-d", strtotime($dateString) + ($displayDays * 24 * 60 * 60))."T23:59:59";
 
-else
-{
-	$dateString = date("Y-m-d")."T00:00:00";
-}
-
-$endDate = date("Y-m-d", strtotime($dateString) + ($displayDays * 24 * 60 * 60))."T12:00:00";
-
- 
-$result = $client->call('GetScheduleInformation', array(
+//Get the schedule from the web service
+$result = $client->GetScheduleInformation(array(
     'ChannelID' => $channelID,
-    'FromDate' => $dateString,
+    'FromDate' => $dateString . "T00:00:00",
     'ToDate' => $endDate,
-    'restrictToShowID' => 0), '', '', false, true);
+    'restrictToShowID' => 0));
 
+// if there are no results or a single result make an array anyway
+// so we can deal with it all the same way below
+if($result->GetScheduleInformationResult->ScheduleInfo == NULL) {
+	$scheduleItems = array();
+} else {
+	$scheduleItems = is_array($result->GetScheduleInformationResult->ScheduleInfo) ?
+		$result->GetScheduleInformationResult->ScheduleInfo :
+		array($result->GetScheduleInformationResult->ScheduleInfo);
+}
 
- 
- 
-// Check for a fault
- 
-if ($client->fault) 
-{
- 
-	echo '<h2>Fault</h2><pre>';
- 
-	print_r($result);
- 
-	echo '</pre>';
- 
-} 
-else 
-{
- 
-	// Check for errors
- 	$err = $client->getError();
- 	if ($err)
-	{
- 		// Display the error
- 		echo '<h2>Error</h2><pre>' . $err . '</pre>';
-	} 
-	else 
-	{
- 		$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
-		$count = 0;
- 		if($resultNumber == '0')
+// Print it all out in a table
+if(count($scheduleItems) == '0') {
+	echo "Nothing is Currently Scheduled for this channel";
+} else {
+	$startDay = "";
+	echo "<table>\n<tr><th>Time</th><th>Program Title</th></tr>\n";
+	foreach($scheduleItems as $run) {
+		$day = date("Y-m-d", strtotime($run->StartTime));
+		if($day != $startDay)
 		{
-			echo "Nothing is Currently Scheduled for this day";
+			echo "<tr><th colspan=\"2\">".date("l F jS, Y", strtotime($day))."</th></tr>\n";
+			$startDay = $day;
 		}
- 
-		if($resultNumber > '1')
-		{ 
-			$startDay = "";
-			echo "<table>\n<tr><th>Time</th><th>Program Title</th></tr>\n";
-			while ($count <= ($resultNumber -1))
-			{
-				$day = date("Y-m-d", strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']));
-				if($day != $startDay)
-				{
-					echo "<tr><th colspan=\"2\">".date("l F jS, Y", strtotime($day))."</th></tr>\n";
-					$startDay = $day;
-				}
- 				echo "<tr><td>".date("g:i a", strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']))."</td><td>".$result['GetScheduleInformationResult']['ScheduleInfo'][$count]['ShowTitle']."</td></tr>\n";
- 				$count++;
-			}
-			echo "</table>\n";
-		}
+			echo "<tr><td>" . date("g:i a", strtotime($run->StartTime)) . "</td><td>" . $run->ShowTitle . "</td></tr>\n";
 	}
+	echo "</table>\n";
 }
  
 ?>
