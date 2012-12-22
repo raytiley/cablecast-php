63635582302e6f9227a289f92fd8f7a9e961b7aa
Ray Tiley <raytiley@gmail.com>
2012-12-22 14:18:11 -0600

    Cleaned up upnext.php
    Removed nusoap dependency and cleaned up script a bit

--- upnext.php
+++ upnext.php
@@ -1,101 +1,45 @@
 <?php
 //Configure Script
-
-$displayUpcomingNum = 4;   // Changes number of upcoming shows to display
-$server = "http://frontdoor.ctn4maine.org/";  //include trailing backslash
+$displayNumber = 4;   // Changes number of upcoming shows to display
+$server = "http://frontdoor.ctn5.org/";  //include trailing backslash
 $channelID = 1;  //Cablecast Channel ID
-
-
+date_default_timezone_set('America/New_York');
 //End Configure
 
-require_once('nusoap.php'); //Script requires NuSoap library
-
-
 $server = $server."CablecastWS/CablecastWS.asmx?WSDL";  //Generates Link to WSDL file.
+$client = new SoapClient($server); //Creates Web Service Client
 
+$result = $client->GetScheduleInformation(array(
+	'ChannelID' => $channelID,
+	'FromDate' => date("Y-m-d") . "T00:00:00",
+	'ToDate' => date("Y-m-d", time() + 60 * 60 * 24) . "T23:59:59",
+	'restrictToShowID' => 0));
 
-$client = new nusoap_client($server, 'wsdl'); //Creates Web Service Client
-
-
-
-
-$offset = 0; // Use if server is in a different timezone.
-$day = 60*60*24;  //Number of seconds in a day.
-$currentDay = date("Y-m-d")."T00:00:00";
-$currentDayTime = date("Y-m-d")."T".date("H:i:s");
-$convertedDayTime = strtotime($currentDayTime);
-$searchTimestr = $convertedDayTime+$day+($offset * 60 * 60);
-$searchTime = date("Y-m-d", $searchTimestr)."T".date("H:i:s", $searchTimestr);
-
-
-
-$result = $client->call('GetScheduleInformation', array(
-'ChannelID' => $channelID,
-'FromDate' => $currentDay,
-'ToDate' => $searchTime,
-'restrictToShowID' => 0), '', '', false, true);
-
-
-
-
-$resultNumber = count($result['GetScheduleInformationResult']['ScheduleInfo']);
-
-
-
-
-
-
-
-
-if($resultNumber > 1)
-{
-$count = 0;
-$startStoring = 0;
-$beginingTime;
-$endingTime;
-$testNumber = 0;
-while ($count <= ($resultNumber - 1))
-{
-$beginingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime']);
-$endingTime = strtotime($result['GetScheduleInformationResult']['ScheduleInfo'][$count]['EndTime']);
-
-
-if(($beginingTime > ($convertedDayTime + ($offset * 60 * 60)))) 
-{
-$testNumber = $count;
-$startStoring = 1;
-
-
+if(!isset($result->GetScheduleInformationResult->ScheduleInfo)) {
+	$schedule = array();
+} else {
+	$schedule = is_array($result->GetScheduleInformationResult->ScheduleInfo) ?
+		$result->GetScheduleInformationResult->ScheduleInfo :
+		array($result->GetScheduleInformationResult->ScheduleInfo);
 }
+$upnext = array();
+if(count($schedule) > 0) {
 
-if($startStoring)
-{
-$comingNextTitle[] = $result['GetScheduleInformationResult']['ScheduleInfo'][$count]['ShowTitle'];
+	foreach($schedule as $run) {
+		$beginingTime = strtotime($run->StartTime);
+		$endingTime = strtotime($run->EndTime);
 
-$comingNextTime[] = $result['GetScheduleInformationResult']['ScheduleInfo'][$count]['StartTime'];
+
+		if($beginingTime > time()) {
+			$upnext[] = array("ShowTitle" => $run->ShowTitle, "StartTime" => $run->StartTime);
+		}
+		if(count($upnext) >= $displayNumber) {
+			break;
+		}
+	}
+	foreach($upnext as $upcomingRun) {
+		// **** ShowTitles could have invalid html characters in them.
+		echo date("g:ia ", strtotime($upcomingRun["StartTime"]));
+		echo $upcomingRun["ShowTitle"] . "\n";
+	}
 }
-
-$count++;
-}
-
-
-}
-
-if(count($comingNextTime) < $displayUpcomingNum)
-{
-$displayUpcomingNum = count($comingNextTime);
-}
-
-$i = 0;
-echo "<table>\n";
-while ($i < $displayUpcomingNum)
-{
-echo "<tr><td valign='top'><b>".date("g:ia ", strtotime($comingNextTime[$i]))."</b> - ".
-$comingNextTitle[$i]."</td></tr>\n";
-$i++;
-}
-echo "</table>\n\n";
-
-
-
-?>
